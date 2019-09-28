6a7fd46989d4132befe1f7dd0eb797e073350d2e
Thiago Bianeck <thiagobianeck@gmail.com>
2019-09-28 14:48:23 -0300

    Finished!!

---
+++ __tests__/unit/user.test.js
@@ -0,0 +1,24 @@
+const { User } = require('../../src/app/models');
+const bcrypt = require('bcryptjs');
+const truncate = require('../utils/truncate');
+
+describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
+  it('should encrypt user password', async () => {
+    const user = await User.create({
+      name: 'Thiago',
+      email: 'thiagobianeck@gmail.com',
+      password: '123456',
+    });
+
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
+  });
+});
+
+
+
